a784a42ded0d9028a1ad31116ce29dc1076217a2
Enrique Salomon <king92salomon@gmail.com>
2021-03-02 22:50:21 +0800

    Add project files.

---
+++ MyApp/PageModels/Base/PageModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyApp.PageModels.Base
+{
+    public class PageModelBase : BindableObject
+    {
+    }
+}
---
+++ MyApp/PageModels/Base/PageModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinyIoC;
+
+namespace MyApp.PageModels.Base
+{
+    public class PageModelLocator
+    {
+        static TinyIoCContainer _container;
+        static Dictionary<Type, Type> _viewLookup;
+    }
+}
